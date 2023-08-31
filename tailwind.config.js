ac3579534e4b3066dd3fb74a1d482cbe0a51f866
sahilmoktan <moktanshail9@gmail.com>
2023-08-31 16:46:10 +0545

    config and nav section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    screens:{
+      sm:'480px',
+      md: '768px',
+      lg:'976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors:{
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSuperLight: 'hsl(12, 88%, 96%)',
+        darkBlue: 'hsl(288, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 13%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+
+      }
+    },
   },
   plugins: [],
 }
